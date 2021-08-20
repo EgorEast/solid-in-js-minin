e4c9ab701624d1a7776348adaccfc156043819c4
EgorEast <jir.jir35@gmail.com>
2021-08-20 15:21:30 +0300

    code with LSP

--- 3_L.js
+++ 3_L.js
@@ -1,28 +1,35 @@
 // Liskov Substitution Principle
 
-class Person {
+class Person {}
+
+class Member extends Person {
 	access() {
 		console.log('У тебя есть доступ');
 	}
 }
 
-class Frontend extends Person {
+class Guest extends Person {
+	isGuest = true;
+}
+
+class Frontend extends Member {
 	canCreateFrontend() {}
 }
 
-class Backend extends Person {
+class Backend extends Member {
 	canCreateBackend() {}
 }
 
-class PersonFromDifferentCompany extends Person {
+class PersonFromDifferentCompany extends Guest {
 	access() {
 		throw new Error('У тебя нет доступа! Иди к себе!');
 	}
 }
 
-function openSecretDoor(person) {
-	person.access();
+function openSecretDoor(member) {
+	member.access();
 }
+
 openSecretDoor(new Frontend());
 openSecretDoor(new Backend());
-openSecretDoor(new PersonFromDifferentCompany());
+// openSecretDoor(new PersonFromDifferentCompany()); // There should be member
