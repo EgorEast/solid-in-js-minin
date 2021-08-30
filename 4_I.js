cedceeb9f9855b3be6a6869edfafb11aa3d697b2
EgorEast <jir.jir35@gmail.com>
2021-08-30 14:04:32 +0300

    code with ISP

--- 4_I.js
+++ 4_I.js
@@ -5,49 +5,43 @@ class Animal {
 	constructor(name) {
 		this.name = name;
 	}
-	walk() {
-		console.log(`${this.name} умеет ходить.`);
-	}
+}
+
+const swimmer = {
 	swim() {
 		console.log(`${this.name} умеет плавать.`);
-	}
+	},
+};
+
+const flier = {
 	fly() {
 		console.log(`${this.name} умеет летать.`);
-	}
-}
+	},
+};
 
-class Dog extends Animal {
-	fly() {
-		return null;
-	}
-}
-
-class Eagle extends Animal {
-	swim() {
-		return null;
-	}
-}
-
-class Whale extends Animal {
-	fly() {
-		return null;
-	}
+const walker = {
 	walk() {
-		return null;
-	}
-}
+		console.log(`${this.name} умеет ходить.`);
+	},
+};
+
+class Dog extends Animal {}
+
+class Eagle extends Animal {}
+
+class Whale extends Animal {}
+
+Object.assign(Dog.prototype, swimmer, walker);
+Object.assign(Eagle.prototype, flier, walker);
+Object.assign(Whale.prototype, swimmer);
 
 const dog = new Dog('Рэкс');
 dog.walk();
 dog.swim();
-dog.fly(); // это не правда
 
 const eagle = new Eagle('Орёл');
 eagle.fly();
-eagle.swim(); // это не правда
 eagle.walk();
 
 const whale = new Whale('Большой синий друг');
-whale.fly(); // это не правда
 whale.swim();
-whale.walk(); // это не правда
